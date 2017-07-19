f9dcc74097b09d2082952fe3c483f298c1020763
emilesu <emilesu@qq.com>
2017-07-19 19:09:36 +0800

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 # ------------------------
@@ -22,6 +23,9 @@ Rails.application.routes.draw do
       delete "/reservations/:booking_code" => "reservations#destroy", :as => :cancel_reservation
     end
   end
+
+# ------------------------
+  root "welcome#index"
 
 
 end
